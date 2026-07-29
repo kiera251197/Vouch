4cd1d704f49583220a6d0c10fec192f60944611e
kiera251197 <251197@virtualwindow.co.za>
2026-07-29 17:17:03 +0200

    Updated naming for dashboards

--- frontend/pages/dashboardSingle.php
+++ frontend/pages/dashboardSingle.php
@@ -6,3 +6,25 @@ error_reporting(E_ALL);
     <br> I am trying my best for this to be done :')</h1>";
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vouch Single's Dashboard</title>
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Google+Sans+Flex:opsz,wght@6..144,1..1000&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="dashboardSingle.css">
+</head>
+
+<body>
+
+</body>
+</html>
