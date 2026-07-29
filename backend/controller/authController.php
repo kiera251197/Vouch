39bf05271cd2b0732b6db7724f0c7961a98d2f7a
kiera251197 <251197@virtualwindow.co.za>
2026-07-29 17:04:13 +0200

    Added error display on authController

--- backend/controller/authController.php
+++ backend/controller/authController.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../models/user.php';
 
--- frontend/pages/dashboardSingle.php
+++ frontend/pages/dashboardSingle.php
@@ -2,6 +2,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-    echo "<h1 style='color: pink;'>This is the dashboardSingle.php page</h1>";
+    echo "<h1 style='color: pink;'>This is the dashboardSingle.php page.
+    <br> I am trying my best for this to be done :')</h1>";
 ?>
 
